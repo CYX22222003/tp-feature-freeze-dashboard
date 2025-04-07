88ac121535e025b64d1d9af143690eb4b1dd2177
CHEN YIXUN <138369841+CYX22222003@users.noreply.github.com>
2025-04-07 14:08:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
-./get-reposense.py --commit a0e771ee53d0cae52d244c893b134010aa4fde9e
+./get-reposense.py --master
 java -jar RepoSense.jar --config ./config --since 05/04/2025  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
 
